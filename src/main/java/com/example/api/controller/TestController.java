26e955d65149ef4452005d5632ab9c48059b864f
ak1473748 <ak1473748@gmail.com>
2025-03-22 16:10:32 +0530

    first commit

---
+++ src/main/java/com/example/api/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.api.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public ResponseEntity<String> test(HttpServletRequest servletRequest){
+        return ResponseEntity.ok().body("Test Api");
+    }
+}
